8ba3623291b57f10bbff8eb21ff237f32de78786
Grzegorz Kolpuc <grzegorz.kolpuc@thomsonreuters.com>
2016-03-21 10:07:46 +0100

    todo

--- hbase-exercises/src/main/java/com/getindata/hbase/exercises/ScannerExercise.java
+++ hbase-exercises/src/main/java/com/getindata/hbase/exercises/ScannerExercise.java
@@ -2,6 +2,9 @@ package com.getindata.hbase.exercises;
 
 public class ScannerExercise {
 	public static void main(String[] args) {
+		// ================================
+		// TODO put your code here
 
+		// ================================
 	}
 }
